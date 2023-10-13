c9a35a4aee66dbcaaa148d037c3e31d0b8aa0636
snehil <108514128+snehilgamit@users.noreply.github.com>
2023-10-13 14:58:43 +0530

    init

--- pages/api/createLink.js
+++ pages/api/createLink.js
@@ -3,31 +3,35 @@ import shortner from "@/models/shortner";
 
 export default async function handler(req, res) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const {link} = req.body;
     await ConnectDB();
+    if (req.body && req.body.link) {
+        const { link } = req.body;
+        if (link.toString().trim().length === 0) {
 
-    if(link.trim().length!==0){
+            function generateString(length) {
+                let result = '';
+                const charactersLength = characters.length;
+                for (let i = 0; i < length; i++) {
+                    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+                }
 
+                return result;
+            }
+            let Shortlink = generateString(8);
+            let findDublicate = await shortner.find({ Shortlink });
 
-    function generateString(length) {
-        let result = '';
-        const charactersLength = characters.length;
-        for (let i = 0; i < length; i++) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            while (!findDublicate) {
+                Shortlink = generateString(8);
+                findDublicate = await shortner.find({ Shortlink });
+            }
+            const upload = await shortner.create({ link, Shortlink });
+            res.json({ Shortlink });
         }
-
-        return result;
+        else {
+            res.json({ err: "provide link" })
+        }
     }
-    let Shortlink = generateString(8);
-    let findDublicate = await shortner.find({Shortlink});
-
-    while(!findDublicate){
-        Shortlink = generateString(8);
-        findDublicate = await shortner.find({Shortlink});
-    }
-    const upload = await shortner.create({link,Shortlink});
-    res.json({Shortlink});}
-    else{
-        res.json({err:"provide link"})
+    else {
+        res.json({ err: "provide link" })
     }
 }
