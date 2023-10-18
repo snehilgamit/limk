695d8a8fc9e6bf2df221e57e4638a82ef6905d1f
snehil <108514128+snehilgamit@users.noreply.github.com>
2023-10-18 12:59:12 +0530

    init

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 
 const Home = () => {
+    const router = useRouter();
     const [link, setlink] = useState('');
     const [shorted,setshored]=useState('');
     const short = async () => {
@@ -24,7 +25,13 @@ const Home = () => {
     }
     return (
         <>
+        <div className="gap-3 cursor-pointer"><div className="px-2 bg-white" onClick={()=>{router.push('/main.docx')}}>DBMS</div>
+            <div className="px-2 bg-white" onClick={()=>{router.push('/7.docx')}}>7 practical</div>
+            <div className="px-2 bg-white" onClick={()=>{router.push('/8.docx')}}>8 practical</div>
+            <div className="px-2 bg-white" onClick={()=>{router.push('/9.docx')}}>9 practical</div></div>
         <div className="w-full min-h-screen bg-white bg-opacity-10 flex justify-center items-center">
+
+            
             <div className="text-black flex justify-center flex-col items-center">
                 <input className="rounded-3xl h-[2rem] w-64 px-2" type="url" name="url" placeholder="Enter link" onChange={(e) => setlink(e.target.value)}/>
                 <div className="text-black mt-4 text-lg bg-white rounded-3xl px-6 hover:bg-opacity-50" onClick={short}>short it</div>
