c7e50d4c16bc351e3641e4dcd071104868d4d322
snehil <108514128+snehilgamit@users.noreply.github.com>
2024-01-23 18:55:07 +0530

    init

--- pages/api/createLink.js
+++ pages/api/createLink.js
@@ -2,8 +2,9 @@ import ConnectDB from "@/helper/mongoDB";
 import shortner from "@/models/shortner";
 
 export default async function handler(req, res) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    await ConnectDB();
+    if(req.method==='POST'){
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        await ConnectDB();
         const { link ,username } = req.body;
         if (link.toString().trim().length !== 0) {
             function generateString(length) {
@@ -12,20 +13,20 @@ export default async function handler(req, res) {
                 for (let i = 0; i < length; i++) {
                     result += characters.charAt(Math.floor(Math.random() * charactersLength));
                 }
-
+                
                 return result;
             }
             let Shortlink = generateString(6);
             let findDublicate = await shortner.find({ Shortlink });
-
+            
             while (!findDublicate) {
                 Shortlink = generateString(6);
                 findDublicate = await shortner.find({ Shortlink });
             }
             const upload = await shortner.create({ link, Shortlink,username });
-            res.json({ Shortlink });
+            return res.json({ Shortlink });
         }
-        else {
-            res.json({ err: "provide link" })
-        }
+        return res.json({ err: "provide link" });
+    }
+    return res.json({err:"Invalid request"});
 }
--- pages/api/remove.js
+++ pages/api/remove.js
@@ -2,16 +2,19 @@ import ConnectDB from "@/helper/mongoDB";
 import shortner from "@/models/shortner";
 
 export default async function handler(req, res) {
-    await ConnectDB();
-        const { link ,username } = req.body;
-        console.log(link,username)
-        if(link && username){
-            const getLink = await shortner.findOne({Shortlink:link,username});
-            if(getLink){
-                await shortner.deleteOne({ Shortlink:link , username })
-                return res.json({status:true,message:"deleted"});
+    if (req.method === "POST") {
+        await ConnectDB();
+        const { link, username } = req.body;
+        console.log(link, username)
+        if (link && username) {
+            const getLink = await shortner.findOne({ Shortlink: link, username });
+            if (getLink) {
+                await shortner.deleteOne({ Shortlink: link, username })
+                return res.json({ status: true, message: "deleted" });
             }
             return res.json({ err: "Invalid link or user" })
         }
         return res.json({ err: "Invalid link or user" })
+    }
+    return res.json({ err: "Invalid request" });
 }
