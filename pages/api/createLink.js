f2a33ebba8278d9b1ff217dfc7175adb943c30e0
snehil <108514128+snehilgamit@users.noreply.github.com>
2024-02-25 00:46:09 +0530

    🔴 FORMAT : readability

--- pages/api/createLink.js
+++ pages/api/createLink.js
@@ -2,31 +2,32 @@ import ConnectDB from "@/helper/mongoDB";
 import shortner from "@/models/shortner";
 
 export default async function handler(req, res) {
-    if(req.method==='POST'){
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    function generateString(length) {
+        let result = '';
+        const charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+
+        return result;
+    }
+
+    if (req.method === 'POST') {
         await ConnectDB();
-        const { link ,username } = req.body;
+        const { link, username } = req.body;
         if (link.toString().trim().length !== 0) {
-            function generateString(length) {
-                let result = '';
-                const charactersLength = characters.length;
-                for (let i = 0; i < length; i++) {
-                    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-                }
-                
-                return result;
-            }
             let Shortlink = generateString(6);
             let findDublicate = await shortner.find({ Shortlink });
-            
+
             while (!findDublicate) {
                 Shortlink = generateString(6);
                 findDublicate = await shortner.find({ Shortlink });
             }
-            const upload = await shortner.create({ link, Shortlink,username });
+            const upload = await shortner.create({ link, Shortlink, username });
             return res.json({ Shortlink });
         }
         return res.json({ err: "provide link" });
     }
-    return res.json({err:"Invalid request"});
+    return res.json({ err: "Invalid request" });
 }
