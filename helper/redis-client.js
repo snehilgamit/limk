d8a319c91640d5ffbef39998cc3f66fb40d2b8d6
disciple.dev <snehilgamit07@gmail.com>
2025-07-19 01:17:31 +0530

    error: redis uri

--- helper/redis-client.js
+++ helper/redis-client.js
@@ -1,13 +1,14 @@
 const { Redis } = require("ioredis");
 
-const redisClient = Redis.createClient({
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-    socket: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
-    }
-})
+const redisClient = Redis.createClient(process.env.REDIS_URI)
+// const redisClient = Redis.createClient({
+//     username: process.env.REDIS_USERNAME,
+//     password: process.env.REDIS_PASSWORD,
+//     socket: {
+//         host: process.env.REDIS_HOST,
+//         port: process.env.REDIS_PORT
+//     }
+// })
 
 export default redisClient
 
